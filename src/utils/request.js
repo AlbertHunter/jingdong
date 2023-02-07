36f3858eba67c2d15fe4f1ba1f075915b7056a6d
feng <feng@lan.com>
2023-02-08 00:20:08 +0800

    首页附近店铺

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,25 @@
 import axios from 'axios'
 
+const instance = axios.create({
+  baseURL: 'https://www.fastmock.site/mock/1074d01602791519c73ab080f17bd948/jd-api',
+  timeout: 10000
+})
+
+export const get = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    instance.get(url, { params }, {
+      baseURL: 'https://www.fastmock.site/mock/1074d01602791519c73ab080f17bd948/jd-api'
+    }).then((response) => {
+      resolve(response.data)
+    }).catch((err) => {
+      reject(err)
+    })
+  })
+}
+
 export const post = (url, data = {}) => {
   return new Promise((resolve, reject) => {
-    axios.post(url, data, {
+    instance.post(url, data, {
       baseURL: 'https://www.fastmock.site/mock/1074d01602791519c73ab080f17bd948/jd-api',
       headers: {
         'Content-Type': 'application/json'
--- src/views/home/NearBy.vue
+++ src/views/home/NearBy.vue
@@ -2,52 +2,37 @@
       <div class="nearby">
         <h3 class="nearby__title">附近店铺</h3>
         <div class="nearby__item" v-for="item in nearbyList" :key="item.id">
-          <img :src="item.imgurl" class="nearby__item__img"/>
+          <img :src="item.imgUrl" class="nearby__item__img"/>
           <div class="nearby__content">
-            <div class="nearby__content__title">{{ item.title }}</div>
+            <div class="nearby__content__title">{{ item.name }}</div>
             <div class="nearby__content__tags">
-              <span class="nearby__content__tag" v-for="(row,innerIndex) in item.tags" :key="innerIndex">{{ row }}</span>
+              <span class="nearby__content__tag">月售：{{ item.sales }}</span>
+              <span class="nearby__content__tag">起送：{{ item.expressLimit }}</span>
+              <span class="nearby__content__tag">基础运费：{{ item.expressPrice }}</span>
             </div>
-            <p class="nearby__content__highlight">{{item.desc}}</p>
+            <p class="nearby__content__highlight">{{item.slogan}}</p>
           </div>
         </div>
       </div>
 </template>
 <script>
+import { ref } from 'vue'
+import { get } from '../../utils/request'
+const useNearbyListEffect = () => {
+  const nearbyList = ref([])
+  const getNearByList = async () => {
+    const result = await get('/api/shop/hot-list')
+    if (result?.code === 200 && result?.data?.length) {
+      nearbyList.value = result.data
+    }
+  }
+  return { nearbyList, getNearByList }
+}
 export default {
   name: 'NearBy',
   setup () {
-    const nearbyList = [{
-      id: 1,
-      imgurl: require('@/assets/images/near.png'),
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }, {
-      id: 2,
-      imgurl: require('@/assets/images/near.png'),
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }, {
-      id: 3,
-      imgurl: require('@/assets/images/near.png'),
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }, {
-      id: 4,
-      imgurl: require('@/assets/images/near.png'),
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }, {
-      id: 5,
-      imgurl: require('@/assets/images/near.png'),
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }]
+    const { nearbyList, getNearByList } = useNearbyListEffect()
+    getNearByList()
     return { nearbyList }
   }
 }
